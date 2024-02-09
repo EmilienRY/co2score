f43bfc8ad873e5af668017e292766f0e91770e9d
EmilienRY <emilien.rey1@gmail.com>
2024-02-09 16:49:42 +0100

    modif page principale et ajout de la page de la liste des plats

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:flutter/services.dart';
+import 'menu.dart';
 
-void main() {
+void main() {   //fonction main de l'app
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(const MyApp());
+  runApp(const MyApp());  //lance l'application
 }
 
 class MyApp extends StatelessWidget {
@@ -18,21 +19,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CO2Score',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'CO2Score'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
 
   final String title;
 
@@ -41,42 +39,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  //mettre variables réutilisés après ici
+
   final GlobalKey qrKey= GlobalKey(debugLabel: 'QR');
   Barcode? result;
   QRViewController? controller;
-  
-  @override
-  void reassemble(){
-    super.reassemble();
-    controller!.resumeCamera();
 
-  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context){   //fonction de création de la page, rajouter des choses ici si on veut modif ce à quoi ressemble la page
     return Scaffold(
+
+        appBar: PreferredSize(     //bar du haut
+          preferredSize: Size.fromHeight(100.0),
+          child: AppBar(
+            backgroundColor: Colors.green,    //changer couleur
+            centerTitle: true,
+            title: const Text('CO2 Score'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add), //A CHANGER
+                onPressed: () {
+                  controller?.pauseCamera(); // arréter la cam quand on change de page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => pageMenu()), // passe à la page avec la liste de tout les plats
+                  ).then((value) {
+
+                    controller?.resumeCamera();  // Reprendre la cam quand on va sur cette page
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+
+
       body: Column(
         children: <Widget> [
-        Expanded(
-          flex: 5,
-          child: QRView(key: qrKey, onQRViewCreated: _onQrViewCreated)
-        ),
-        Expanded(
-            flex: 1,
-          child: Center(
-              child: (result != null) ? Text("Barcode Type: ${result!.format.name} Data: ${result!.code} ") : const Text("Scan a code")
-            ),
+
+          Expanded(
+            flex: 5,
+            child: QRView(key: qrKey, onQRViewCreated: _onQrViewCreated)   //emplacement du scanner QRcode
+          ),
+          Expanded(
+              flex: 1,
+            child: Center(
+                child: (result != null) ? Text("Barcode Type: ${result!.format.name} Data: ${result!.code} ") : const Text("Scan a code")  // affiche les valeurs scan pour le moment
+              ),
         )
       ],),
+
     );
   }
   
-  void _onQrViewCreated(QRViewController controller){
+  void _onQrViewCreated(QRViewController controller){   //vonction qui initialize le scanner et passe en écoute. Stocke valeur du scan dans variable result
     this.controller=controller;
     controller.scannedDataStream.listen((scanData) { 
       setState(() {
         result=scanData;
       });
     });
+  }
+
+  @override
+  void reassemble(){
+    super.reassemble();
+    controller!.resumeCamera();
+
   }
   
   @override
---
+++ lib/menu.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class pageMenu extends StatelessWidget {   //page avec la liste des plats
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('page des menus'),
+      ),
+    );
+  }
+}
