acf2e1de574f89f22230de8e786f7fb41c8a15f0
EmilienRY <emilien.rey1@gmail.com>
2024-02-11 18:44:23 +0100

    ajout de la page de visualisation des plats et de création

---
+++ lib/CreationPlat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+class pageCreation extends StatelessWidget {   //page avec la liste des plats
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Créer votre plat'),
+      ),
+
+    );
+  }
+}
--- lib/menu.dart
+++ lib/menu.dart
@@ -1,14 +1,92 @@
 import 'package:flutter/material.dart';
-
-
+import 'CreationPlat.dart';
+import 'visuPlat.dart';
 
 class pageMenu extends StatelessWidget {   //page avec la liste des plats
+
+  final List<String> buttonTexts = ['Page 1', 'Page 2', 'Page 3'];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('page des menus'),
       ),
+
+      body: Center(
+      child :Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => pageCreation()), // Passe à la page avec la liste de tous les plats
+              );
+            },
+            child: Text('Ajouter un plat'),
+          ),
+          SizedBox(height: 20), // Espacement entre le bouton et le contenu suivant
+          Text(
+            'Contenu sous le bouton',
+            style: TextStyle(fontSize: 18),
+          ),
+          Column(children: buttonTexts.map(
+                  (text) => ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => pageVisu(),
+                    ),
+                  );
+                },
+                child: Text(text),
+              ),
+            ).toList(),
+          ),
+        ],
+        ),
+
+
+      ),
+
+
+
+
+     /* body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => pageCreation()), // passe à la page avec la liste de tout les plats
+            );
+          },
+          child: Text('Ajouter un plat'),
+        )
+      )
+
+
+      body: Center(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: buttonTexts.map(
+                (text) => ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => pageVisu(),
+                        ),
+                      );
+                    },
+                    child: Text(text),
+                  ),
+                ).toList(),
+    ),
+    )  */
+
     );
   }
 }
