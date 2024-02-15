32e8c2c5bbd810e46e72d971e3cb45ed730f796e
Paul Deligne <delopolo23@gmail.com>
2024-02-15 16:04:23 +0100

    Ajout bouton création plat

--- lib/CreationPlat.dart
+++ lib/CreationPlat.dart
@@ -7,9 +7,36 @@ class pageCreation extends StatelessWidget {   //page avec la liste des plats
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Créer votre plat'),
+        title:  Text('Créer votre plat'),
       ),
 
+    body: const Column(
+      children: <Widget> [
+      Expanded(
+      flex: 5,
+        child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Ingrédient 1',
+            ),
+        ),),
+      ElevatedButton(
+        onPressed: null,
+        child: const Text('Disabled'),
+    ),
+    const SizedBox(height: 30),
+      ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => pageCreation()), // Passe à la page avec la liste de tous les plats
+        );
+      },
+    child: const Text('Enabled'),
+    ),
+    ],
+    ),
     );
-  }
-}
+}}
+
