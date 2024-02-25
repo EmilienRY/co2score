0d188e25a312d6360dfbc8a32e068877591644d3
PaulDeligne <delopolo23@gmail.com>
2024-02-25 14:28:54 +0100

    Correction bug bouton + début fonction pour ajouter des ingrédients à un plat

--- lib/CreationPlat.dart
+++ lib/CreationPlat.dart
@@ -1,42 +1,62 @@
 import 'package:flutter/material.dart';
-
+import 'menu.dart';
 
 
 class pageCreation extends StatelessWidget {   //page avec la liste des plats
   @override
+
+    final  TextField ingredient=TextField(
+    obscureText: true,
+    decoration: InputDecoration(
+      border: OutlineInputBorder(),
+      labelText: 'Ingrédient',
+    ),
+  );
+  final List <TextField> ListIngredient =[TextField(
+  obscureText: true,
+  decoration: InputDecoration(
+  border: OutlineInputBorder(),
+  labelText: 'Ingrédient',
+  ),
+  ),];
   Widget build(BuildContext context) {
+    void _ajoutIngredient(){
+      ListIngredient.add(ingredient);
+      print(ListIngredient);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => pageCreation()), // Passe à la page avec la liste de tous les plats
+      );
+      }
+      void _supIngredient(){
+      ListIngredient.remove(ListIngredient.last);
+      }
+
     return Scaffold(
       appBar: AppBar(
         title:  Text('Créer votre plat'),
       ),
-
-    body: const Column(
-      children: <Widget> [
-      Expanded(
-      flex: 5,
-        child: TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Ingrédient 1',
-            ),
-        ),),
+      body: Center(
+      child:Column(
+      children: [
+      Column(children: ListIngredient),
+      Column(
+        children: [
+      const SizedBox(height: 30),
+        ElevatedButton(
+        onPressed: () {
+          _ajoutIngredient();
+        },
+      child: const Text('Ajouter un ingrédient'),
+      ),
+      const SizedBox(height: 30),
       ElevatedButton(
-        onPressed: null,
-        child: const Text('Disabled'),
-    ),
-    const SizedBox(height: 30),
-      ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => pageCreation()), // Passe à la page avec la liste de tous les plats
-        );
-      },
-    child: const Text('Enabled'),
-    ),
-    ],
-    ),
-    );
+        onPressed: () {
+          _supIngredient();
+        },
+        child: const Text('Supprimer un ingrédient'),
+      ),
+      ],
+      ),
+      ],),),);
 }}
-
