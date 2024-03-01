91dd129fe1e52d61badbe90785a4343ac709c010
EmilienRY <emilien.rey1@gmail.com>
2024-03-01 13:10:10 +0100

    modification de menu.dart pour afficher les plats contenus dans le fichier csv

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,11 +1,33 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'CreationPlat.dart';
 import 'visuPlat.dart';
+import 'package:csv/csv.dart';
 
-class pageMenu extends StatelessWidget {   //page avec la liste des plats
+class pageMenu extends StatefulWidget {
+  @override
+  _pageMenuState createState() => _pageMenuState();
+}
 
-  final List<String> buttonTexts = ['Page 1', 'Page 2', 'Page 3','page4', 'Page 3', 'Page 3', 'Page 3', 'Page 3', 'Page 3'];
+class _pageMenuState extends State<pageMenu> {
+  List<String> buttonTexts = [];
 
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final String csvString = await rootBundle.loadString('assets/plat.csv');
+    final List<List<dynamic>> fields = CsvToListConverter().convert(csvString);
+    // Assuming the first column contains the dish names
+    setState(() {
+      for (final row in fields) {
+        buttonTexts.add(row[0]);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,46 +35,45 @@ class pageMenu extends StatelessWidget {   //page avec la liste des plats
       appBar: AppBar(
         title: Text('page des menus'),
       ),
-
       body: Center(
-      child :Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => pageCreation()), // Passe à la page avec la liste de tous les plats
-              );
-            },
-            child: Text('Ajouter un plat'),
-          ),
-          SizedBox(height: 20), // Espacement entre le bouton et le contenu suivant
-          Text(
-            'Contenu sous le bouton',
-            style: TextStyle(fontSize: 18),
-          ),
-          Column(children: buttonTexts.map(
-                  (text) => ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => pageVisu(),
-                    ),
-                  );
-                },
-                child: Text(text),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => pageCreation()),
+                );
+              },
+              child: Text('Ajouter un plat'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Contenu sous le bouton',
+              style: TextStyle(fontSize: 18),
+            ),
+            if (buttonTexts.isNotEmpty)
+              Column(
+                children: buttonTexts.map(
+                      (text) => ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => pageVisu(),
+                        ),
+                      );
+                    },
+                    child: Text(text),
+                  ),
+                ).toList(),
               ),
-            ).toList(),
-          ),
-        ],
+            if (buttonTexts.isEmpty)
+              CircularProgressIndicator(), // Affichez une indication de chargement tant que les données ne sont pas chargées
+          ],
         ),
-
-
       ),
-
-
     );
   }
 }
